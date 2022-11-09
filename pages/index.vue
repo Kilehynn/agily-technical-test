d488b49cd83850af3bce631fae31661f50d2647a
mexane.delcroix <mexane.delcroix@epita.fr>
2022-11-09 16:14:37 +0100

    feat(welcomePage): add title

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="container">
-    <div class="main"></div>
+    <div class="main">
+      <h1>The Forecast<br>Weather App</h1>
+
+    </div>
   </div>
 </template>
-<style lang="scss"></style>
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'IndexPage',
-})
+  name : 'IndexPage',
+
+});
 </script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: var(--primary-color);
+}
+
+.main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  gap: 35px;
+}
+h1 {
+  background: -webkit-linear-gradient(#25c25c, #e6e99f);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+
+
+
+
+</style>
