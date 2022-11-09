3a28897264ad2775a06a6c78cc69db173ec30b94
mexane.delcroix <mexane.delcroix@epita.fr>
2022-11-09 16:16:10 +0100

    feat(welcomePage): add input

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div class="main">
       <h1>The Forecast<br>Weather App</h1>
-
+      <div class="search-city">
+        <input v-model="city" placeholder="Search">
+      </div>
     </div>
   </div>
 </template>
@@ -11,7 +13,11 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name : 'IndexPage',
-
+  data() {
+    return {
+      city : ''
+    };
+  }
 });
 </script>
 
@@ -30,14 +36,35 @@ export default Vue.extend({
 
   gap: 35px;
 }
+
 h1 {
   background: -webkit-linear-gradient(#25c25c, #e6e99f);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
 
+.search-city > input {
+  width: 20em;
+  height: 50px;
+  color: var(--primary-color);
+  font-family: 'Poppins Black', sans-serif;
+  padding: 15px;
+  border-radius: 15px;
+
+  border: none;
+  outline: none;
+
+  font-weight: 700;
+
+  &::placeholder {
+    color: var(--primary-color);
+  }
+}
 
 
-
-
+@media screen and (min-width: 481px) {
+  input {
+    width: 35em;
+  }
+}
 </style>
