3d3b7baba6b986c85cbdef26bb4364f57e51fd5a
mexane.delcroix <mexane.delcroix@epita.fr>
2022-11-09 16:17:50 +0100

    feat(welcomePage): add submit button

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,18 @@
       <h1>The Forecast<br>Weather App</h1>
       <div class="search-city">
         <input v-model="city" placeholder="Search">
+        <button @submit="onCitySubmitted" class="search-city-submit" type="submit">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <script xmlns=""/>
+            <path fill="none" stroke="#000" stroke-width="36" stroke-linecap="round"
+                  d="m280,278a153,153 0 1,0-2,2l170,170m-91-117 110,110-26,26-110-110"/>
+          </svg>
+        </button>
       </div>
     </div>
   </div>
 </template>
+
 <script lang="ts">
 import Vue from 'vue';
 
@@ -65,6 +73,33 @@ h1 {
 @media screen and (min-width: 481px) {
   input {
     width: 35em;
+  }
+}
+
+
+.search-city {
+
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  height: min-content;
+}
+
+.search-city-submit {
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+
+  top: 55%;
+  right: 5px;
+  transform: translateY(-50%);
+  position: absolute;
+
+  & svg {
+    width: 20px;
+    height: 20px;
   }
 }
 </style>
