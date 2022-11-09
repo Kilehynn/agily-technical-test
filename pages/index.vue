0d07dc4f76268a64fc92c9ffcd037cc0cc3e3482
mexane.delcroix <mexane.delcroix@epita.fr>
2022-11-09 14:04:35 +0100

    fix(index): fix linter main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
+  <div class="container">
+    <div class="main"></div>
+  </div>
 </template>
-
+<style lang="scss"></style>
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
 
 export default Vue.extend({
-  name : 'IndexPage',
-});
+  name: 'IndexPage',
+})
 </script>
